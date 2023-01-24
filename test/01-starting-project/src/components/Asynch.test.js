13fe45e27255d906873acf968fcb51228f715c8b
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-01-24 21:20:39 +0530

    mock testing

--- test/01-starting-project/src/components/Asynch.test.js
+++ test/01-starting-project/src/components/Asynch.test.js
@@ -4,6 +4,10 @@ import Async from "./Asynch"
 
 describe("Asynch component",()=>{
     test("render post if request success",async()=>{
+        window.fetch=jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async()=>[{id:"p1",title:"First post"}]
+        })
         render(<Async/>)
         const listItemElements=await screen.findAllByRole("listitem");
         expect(listItemElements).not.toHaveLength(0)
